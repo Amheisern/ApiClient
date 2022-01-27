f4fc49752ec84a20f163f589033c59f248a3ed44
lodashreload <icarusjapan@gmail.com>
2022-01-26 19:31:17 -0500

    added display greeting, promptforstring, promptforinteger, methods and extracted  ShowAllPeople method

--- Program.cs
+++ Program.cs
@@ -9,22 +9,74 @@ namespace GhibliStatus
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void DisplayGreeting()
         {
-            var client = new HttpClient();
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("         Welcome to the Ghibli database");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+        }
 
-            var responseAsStream = await client.GetStreamAsync("https://ghibliapi.herokuapp.com/people/");
-            //Console.WriteLine(responseAsStream);
-            var peoples = await JsonSerializer.DeserializeAsync<List<People>>(responseAsStream);
-            var table = new ConsoleTable("Name", "Gender", "Age", "Eye Color", "Eye Color");
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
 
-            foreach (var people in peoples)
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
             {
-                table.AddRow(people.Name, people.Gender, people.Age, people.EyeColor, people.HairColor);
-                //Console.WriteLine($"The character {people.Name} has {people.EyeColor} eyes and {people.HairColor} hair.");
+                return userInput;
             }
-            table.Write();
-            //Console.WriteLine(responseAsString);
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+
+            static async Task Main(string[] args)
+            {
+
+
+                var keepGoing = true;
+
+                DisplayGreeting();
+
+                while (keepGoing)
+                {
+
+                }
+
+
+
+
+                await ShowAllPeople();
+                //Console.WriteLine(responseAsString);
+            }
+
+            static async Task ShowAllPeople()
+            {
+                var client = new HttpClient();
+
+                var responseAsStream = await client.GetStreamAsync("https://ghibliapi.herokuapp.com/people/");
+                //Console.WriteLine(responseAsStream);
+                var peoples = await JsonSerializer.DeserializeAsync<List<People>>(responseAsStream);
+                var table = new ConsoleTable("Name", "Gender", "Age", "Eye Color", "Eye Color");
+
+                foreach (var people in peoples)
+                {
+                    table.AddRow(people.Name, people.Gender, people.Age, people.EyeColor, people.HairColor);
+                    //Console.WriteLine($"The character {people.Name} has {people.EyeColor} eyes and {people.HairColor} hair.");
+                }
+                table.Write();
+            }
         }
     }
 }
